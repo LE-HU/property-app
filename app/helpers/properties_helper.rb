8656933643f21a81fb7f036e5131610d299176e5
LE-HU <wojciechlech1337@gmail.com>
2020-09-15 02:56:56 +0200

    finalize property show layout + agent partial

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,5 +1,10 @@
 module PropertiesHelper
   def property_thumbnail(property)
     img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+    image_tag img, class: "my-4"
+  end
+
+  def property_photo_url(property)
+    property.photo.present? ? property.photo.url : asset_url("placeholder.png")
   end
 end
